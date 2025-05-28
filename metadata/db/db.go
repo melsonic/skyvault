2d2c520fd35fb37294706e485ed8f5720c9f0c8c
melsonic <work.melsonic@gmail.com>
2025-05-29 00:14:01 +0530

    support folders in metadata

--- metadata/db/db.go
+++ metadata/db/db.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/jackc/pgx"
 	"github.com/jackc/pgx/pgtype"
@@ -12,7 +13,9 @@ import (
 )
 
 var (
-	DBConn *pgx.Conn
+	DBConn      *pgx.Conn
+	ROOT_FOLDER = -1
+	ROOT_NAME   = "root"
 )
 
 func connectDB() error {
@@ -35,15 +38,14 @@ func connectDB() error {
 
 func setupDB() error {
 	_, err := DBConn.Exec(`
-		CREATE TABLE IF NOT EXISTS FILE (
+		CREATE TABLE IF NOT EXISTS NODE (
 			ID bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-			FILE_NAME text NOT NULL,
+			FOLDER boolean NOT NULL,
+			NAME text NOT NULL,
+			PARENT_FOLDER bigint,
 			HASH_IDS text[]
 		)
 	`)
-	if err != nil {
-		return err
-	}
 	return err
 }
 
@@ -57,6 +59,29 @@ func InitDB() error {
 }
 
 func SaveMetadata(data *types.Metadata) error {
+	path := strings.Split(data.FilePath, "/")
+	var parentFolderId int
+	var index int = 0
+	for index = 0; index < len(path); index++ {
+		var tempParentId int
+		err := DBConn.QueryRow("SELECT ID FROM NODE WHERE NAME = $1", path[index]).Scan(&tempParentId)
+		if err != nil {
+			break
+		}
+		parentFolderId = tempParentId
+	}
+	for index < len(path) {
+		var tempParentId int
+		err := DBConn.QueryRow("INSERT INTO NODE (FOLDER, NAME, PARENT_FOLDER) VALUES ($1, $2, $2) RETURNING ID", true, path[index], parentFolderId).Scan(&tempParentId)
+		if err != nil {
+			return err
+		}
+		parentFolderId = tempParentId
+		index++
+	}
+	if data.IsFolder {
+		return nil
+	}
 	hashes := "{"
 	for i := range data.Hashes {
 		hashes += fmt.Sprintf(`"%s"`, data.Hashes[i])
@@ -66,7 +91,7 @@ func SaveMetadata(data *types.Metadata) error {
 		hashes += ","
 	}
 	hashes += "}"
-	_, err := DBConn.Exec(`INSERT INTO FILE VALUES ('$1', '$2')`, data.FileName, hashes)
+	_, err := DBConn.Exec(`INSERT INTO NODE (FOLDER, NAME, PARENT_FOLDER, HASH_IDS) VALUES ($1, '$2', $3, $4)`, data.IsFolder, data.FileName, parentFolderId, hashes)
 	return err
 }
 
--- metadata/types/type.go
+++ metadata/types/type.go
@@ -1,6 +1,10 @@
 package types
 
+// FilePath will not contain the filename
+// If IsFolder is true, FileName & Hashes will be empty
 type Metadata struct {
 	FileName string   `json:"filename"`
+	FilePath string   `json:"filepath"`
+	IsFolder bool     `json:"is_folder"`
 	Hashes   []string `json:"hashes"`
 }
